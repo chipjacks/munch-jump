fd7e20fb4c50b6b3c79305e2de897816c8c83cd3
Chip Jackson <chipjacks@gmail.com>
2024-06-15 14:27:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Munch Jump
+
+Inspired by the game Doodle Jump.
+
+### Develop
+```
+python3 -m http.server
+```
---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(
+    windowWidth,
+    windowHeight,
+    document.getElementById("main-canvas")
+  );
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
